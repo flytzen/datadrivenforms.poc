a177acbd20bc2883e5d051c259b692c71fac5312
Frans Lytzen <flytzen@neworbit.co.uk>
2022-08-15 14:51:49 +0000

    Added an API. This is now a baseline azure function with server.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
 import React from 'react';
+import { useEffect, useState } from 'react';
 
 function App() {
-  const value = 'World';
-  return <div>Hello {value}!</div>;
+  const [data, setData] = useState('...');
+
+  useEffect(() => {
+    (async function () {
+      const a = await fetch(`/api/SayHello`);
+      const text = await a.text();
+      setData(text);
+    })();
+  });
+  
+  return <div>Hello {data}!</div>;
 }
 
 export default App;
